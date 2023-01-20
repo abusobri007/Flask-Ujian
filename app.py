d3ac713ee53decbdf61bf1e4741de1257b3dbb3f
briisco <abusobri007@gmail.com>
2023-01-20 22:05:26 +0800

    feat : membuat list pendaftaran dan tambah peserta

--- app.py
+++ app.py
@@ -45,40 +45,4 @@ def save_mantan():
        db.session.commit()
        return redirect('/list_kayu')
 
-       
-@app.route("/list_kayu/<id>/edit")
-def edit_peserta(id):
-    obj = Kayu.query.filter_by(id=id).first()
-    return render_template("edit_kayu.html",obj=obj)
-
-@app.route("/list_kayu/<id>/update", methods=['POST'])
-def update_peserta(id):
-    obj = Kayu.query.filter_by(id=id).first()#data from db
- #data from cline server
-    f_jenis =request.form.get("jenis")
-    f_ukuran =request.form.get("ukuran")
-    f_berat =request.form.get("berat")
-    f_variabel =request.form.get("variabel")
-    f_harga=request.form.get("harga")    
-    #data form db whiht data from cline server
-    obj.jenis=f_jenis
-    obj.ukuran=f_ukuran
-    obj.berat=f_berat
-    obj.variabel=f_variabel
-    obj.harga=f_harga
-
-    #save perubahan data ke db
-
-    db.session.add(obj)
-    db.session.commit()
-    return redirect('/list_kayu')   
-
-app.route("/list_kayu/<id>/delete")
-def delete_pendafar(id):
-    obj = Kayu.query.filter_by(id=id).first()
-    db.session.delete(obj)
-    db.session.commit()
-
-    return redirect('/list_kayu')
-if "__main__" ==__name__:
-     app.run(debug= True,port =3000)
+ 
